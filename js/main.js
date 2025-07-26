9fc02a85a113df593b7dcd1b056971f46cbb7b29
Abeer Maged <aalgehshry38@gmail.com>
2025-07-26 22:05:57 +0300

    DevFolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+    strings: [
+      'Designer',
+      'Developer',
+      'Freelancer',
+      'Photographer',
+    ],
+
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+})
